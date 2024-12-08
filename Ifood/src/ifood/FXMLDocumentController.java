0c7967204678a4e7784d01ab51e312c00efcf445
JoseVColino <josevictorcolino@gmail.com>
2024-12-08 13:29:25 -0300

    Add files via upload

---
+++ Ifood/src/ifood/FXMLDocumentController.java
@@ -0,0 +1,66 @@
+package ifood;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLDocumentController {
+
+    @FXML
+    private Button CriarContaCliente;
+
+    @FXML
+    private Button CriarContaRestaurante;
+
+    @FXML
+    private Button cadastrarButton;
+
+    @FXML
+    private Button cadastrarCliente;
+
+    @FXML
+    private AnchorPane cadastroCliente;
+
+    @FXML
+    private AnchorPane cadastroRestaurante;
+
+    @FXML
+    private Button entrarButton;
+
+    @FXML
+    private AnchorPane login;
+
+    @FXML
+    private TextField loginTextField;
+
+    @FXML
+    private TextField senhaTextField;
+
+    @FXML
+    void handleCriarContaCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCriarContaRestaurante(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEntrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarCadastroCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarCadastroRestaurante(ActionEvent event) {
+
+    }
+
+}
